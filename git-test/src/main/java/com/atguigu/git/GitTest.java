c57a58923c717bfb303d45b68adc10ff5c408fa3
git <git@atguigu.com>
2021-12-02 16:07:02 +0800

    first commit

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @auther sunqiaozheng
+ * @date 2021/12/2 - 16:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
